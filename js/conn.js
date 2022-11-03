6e66e12f8ad3a2c1c9fec875c74f2972b443482e
unknown <albertoi@20143ALBERTOI.ih.gov.mo>
2022-11-03 15:30:00 +0800

    Host on render.com

--- js/conn.js
+++ js/conn.js
@@ -1,4 +1,10 @@
 
-let HOST_URI = 'http://localhost:3000/posts/'
+// Development environment 
+// let HOST_URI = 'http://localhost:3000/posts/'
+
+// Production environment 
+// Host on render.com
+let HOST_URI = 'https://json-server-tutorial.onrender.com'
+// 2022/11/03 
 
 export { HOST_URI }
